18ad3f23c07b94f27168f3983e2b98c203634eaf
cjfxks <kimkyungyun1116@hanmail.net>
2016-07-21 15:15:01 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -1,10 +1,12 @@
 //For command line git tutorial
-//GitHub modification
+//Local repository
+//GitHub modyfication
 public class Hello{
    public static void main(String[] args){
      System.out.println("Hello Git!");
      System.out.println("What is Git?");
      System.out.println("That is DVC");
      System.out.println("2002, BitKeeper");
+     System.out.println("That's version control");
    }
 }
